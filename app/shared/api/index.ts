82e1b45ce125536fd2741f201082f651128f57c5
Мамизеров Никита <nmamizerov@gmail.com>
2025-10-23 14:27:35 +0300

    add nix

--- app/shared/api/index.ts
+++ app/shared/api/index.ts
@@ -1,17 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "@/core/store";
 
+const getBaseUrl = () => {
+  if (typeof window === "undefined") {
+    return process.env.VITE_API_URL;
+  }
+  if (window.location.host === "localhost") {
+    return "/api/v1";
+  }
+  return `${import.meta.env.VITE_API_URL}/api/v1`;
+};
+
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl:
-      typeof window === "undefined"
-        ? import.meta.env.VITE_API_URL
-        : window.location.host === "localhost"
-          ? "/api/v1"
-          : `${import.meta.env.VITE_API_URL}/api/v1`,
-    credentials: "include",
-  }),
+  baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl(), credentials: "include" }),
   tagTypes: ["User", "Course", "Lessons", "Simulator", "CourseUser"],
 
   endpoints: () => ({}),
